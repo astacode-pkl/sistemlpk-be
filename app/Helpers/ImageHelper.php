130fc849fcece44bb60e9c246af06f633b783053
mrizki <rizkixirpl@gmail.com>
2025-04-08 10:26:48 +0700

    Update image helper menghapus convert

--- app/Helpers/ImageHelper.php
+++ app/Helpers/ImageHelper.php
@@ -2,32 +2,15 @@
 
 namespace App\Helpers;
 
-use Intervention\Image\ImageManager;
-use Intervention\Image\Drivers\Gd\Driver;
-use Intervention\Image\Encoders\WebpEncoder;
 use Illuminate\Support\Facades\Storage;
 
 class ImageHelper
-{
-    protected $manager;
-    public function __construct()
-    {
-        $this->manager = new ImageManager(new Driver());
-    }
+
 
     public static function processImage($imageFile, $path)
     {
-        // Baca & ubah gambar ke WebP
-        $manager = new ImageManager(new Driver());
-        $image = $manager->read($imageFile->getPathname());
-        $webpImage = $image->encode(new WebpEncoder(quality: 90));
-        // Buat nama file unik
-        $imageName = date('YmdHis') . '_' . md5(uniqid()) . '.webp';
-        $fullPath = $path . $imageName;
-
-        // Simpan gambar ke storage/public
-        Storage::disk('public')->put($fullPath, (string) $webpImage);
-
+       // Simpan gambar ke storage/public
+        $fullPath = $imageFile->store($path);
         return $fullPath;
     }
     public static function deleteImage($imagePath)
